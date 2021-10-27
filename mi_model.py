ea275306789027e5d97bb0180040a470091b9151
satyaram413 <maka.sitaram@gmail.com>
2021-10-27 16:04:07 +0530

    ML Model code

--- mi_model.py
+++ mi_model.py
@@ -1,1 +1,8 @@
 print("Will add all model logic here")
+
+def model1():
+    print("Adding code for ML model1")
+def model2():
+    print("Adding code for ML Model 2")
+def model3():
+    print("Adding code for ML Model 3")
